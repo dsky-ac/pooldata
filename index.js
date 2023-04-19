270b7d35296205c1b44ec70ec1c2b10c99509a72
dsky-ac <85622187+dsky-ac@users.noreply.github.com>
2023-04-19 09:13:37 +0000

    update pool

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const rpcUrl = 'https://rpc.bt.io'
+const rpcUrl = 'https://mainnet.era.zksync.io'
 const { ethers } = require("ethers");
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
@@ -8,10 +8,10 @@ const adapter = new FileSync('./pools.json');
 const db = low(adapter);
 //
 const basePrice = {
-  '0xdb28719f7f938507dbfe4f0eae55668903d34a15': 1
+  '0x06ca4F37C4d76A0aF8814f1d814c5811C63B388e': 1
 }
-const rewardToken = '0x6fc441E5C55e4A5DDDdB3E3d7335877E5D20b002';
-const roomAddress = '0x3a1bb191d89ace21518fb01d73c143da2b0c5fae';
+const rewardToken = '0x06ca4F37C4d76A0aF8814f1d814c5811C63B388e';
+const roomAddress = '0xabf06b8455453EA892e87Fef87A2B748985d3B56';
 const rewardPrice = 1
 //
 async function getData() {
